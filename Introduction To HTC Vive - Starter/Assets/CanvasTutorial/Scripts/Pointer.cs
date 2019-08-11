85121a841d3f1d20f611932510e963d8985d0b64
Mário Gil <mg_mesquita@hotmail.com>
2019-08-11 22:42:43 +0100

    Canvas Pointer

---
+++ Introduction To HTC Vive - Starter/Assets/CanvasTutorial/Scripts/Pointer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public float m_DefaultLength = 5.0f;
+    public GameObject m_Dot;
+    public VRInputModule m_InputModule;
+
+    private LineRenderer m_LineRenderer = null;
+
+    private void Awake()
+    {
+        m_LineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        // use default or distance
+        float targetLength = m_DefaultLength;
+
+        // raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        // default
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        // or based on hit
+        if (hit.collider != null)
+            endPosition = hit.point;
+
+        // set dot position
+
+        m_Dot.transform.position = endPosition;
+        // set line renderer position
+        m_LineRenderer.SetPosition(0, transform.position);
+        m_LineRenderer.SetPosition(1, endPosition);
+    }
+
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, m_DefaultLength);
+        return hit;
+    }
+}
---
+++ Introduction To HTC Vive - Starter/Assets/CanvasTutorial/Scripts/VRInputModule.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class VRInputModule : BaseInputModule
+{
+    public Camera m_Camera;
+    public SteamVR_Input_Sources m_TargetSource;
+    public SteamVR_Action_Boolean m_ClickAction;
+
+    private GameObject m_CurrentObject = null;
+    private PointerEventData m_Data = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        m_Data = new PointerEventData(eventSystem);
+    }
+
+    public override void Process()
+    {
+        // reset data, set camera
+        m_Data.Reset();
+        m_Data.position = new Vector2(m_Camera.pixelWidth / 2, m_Camera.pixelHeight / 2);
+
+        // raycast
+        eventSystem.RaycastAll(m_Data, m_RaycastResultCache);
+        m_Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        m_CurrentObject = m_Data.pointerCurrentRaycast.gameObject;
+
+        // clear raycast
+        m_RaycastResultCache.Clear();
+
+        // handle hover
+        HandlePointerExitAndEnter(m_Data, m_CurrentObject);
+
+        // press
+        if (m_ClickAction.GetStateDown(m_TargetSource))
+        {
+            ProcessPress(m_Data);
+        }
+
+        // release
+        if (m_ClickAction.GetStateUp(m_TargetSource))
+        {
+            ProcessRelease(m_Data);
+        }
+    }
+
+    public PointerEventData GetData()
+    {
+        return m_Data;
+    }
+
+    private void ProcessPress(PointerEventData data)
+    {
+
+    }
+
+    private void ProcessRelease(PointerEventData data)
+    {
+
+    }
+}
